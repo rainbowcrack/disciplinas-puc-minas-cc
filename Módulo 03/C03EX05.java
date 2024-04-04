a11adc1898d271048672ac7322cef473a4a04a2a
Izabel <145824500+rainbowcrack@users.noreply.github.com>
2024-04-04 19:36:09 -0300

    Create C03EX05.java

---
+++ Módulo 03/C03EX05.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class C03EX05 {
+    public static void main(String[] args) {
+        // atividade do módulo 03 - exercício 05 por Izabel Oliveira da Paz Chaves (1º período de C.C)
+        // programa que calcula a idade do user no ano inserido, baseado na subtração do nascimento
+        String nome;
+        int anoNasc, anoAtual, idade;
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        nome = teclado.nextLine();
+        System.out.print("Digite o ano em que você nasceu: ");
+        anoNasc = teclado.nextInt();
+        System.out.print("Digite o ano em que estamos: ");
+        anoAtual = teclado.nextInt();
+        idade = anoAtual - anoNasc;
+        System.out.println(nome+", você tem/terá "+idade+" anos em "+anoAtual);
+        teclado.close();
+    }
+}
