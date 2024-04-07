527e01b66dbe7ba3ae8676c154fbfc24f7b4e128
Izabel <145824500+rainbowcrack@users.noreply.github.com>
2024-04-07 14:21:37 -0300

    Create C04EX03.java

---
+++ Módulo 04/C04EX03.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+// atividade do módulo 04 - exercício 02 por Izabel Oliveira da Paz Chaves (1º período de C.C)
+// programa para criar uma tabela sobre o poluente emitido e sua relacao a multa aplicada
+public class C04EX03 {
+    public static void main(String[] args) {
+        // input
+        int p_menor, p_maior;
+        float multa1, multa2, multa3;
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Registre o menor valor do poluente emitido pela empresa: ");
+        p_menor = teclado.nextInt();
+        System.out.print("E o maior tambem: ");
+        p_maior = teclado.nextInt();
+        System.out.print("Insira o valor da multa aplicada para o menor poluente emitido: ");
+        multa1 = teclado.nextFloat();
+        System.out.print("A multa aplicada para o valor intermediario dos poluentes: ");
+        multa2 = teclado.nextFloat();
+        System.out.print("Multa aplicada para o valor acima da maior emissao: ");
+        multa3 = teclado.nextFloat();
+        // output
+        System.out.println("___________________________________________");
+        System.out.print(
+                "Quantidade de Poluente Emitido x Valor da Multa" + "\n___________________________________________");
+        System.out.printf("\nAté " + p_menor + " multa de R$%1.2f", multa1);
+        System.out.print("\n___________________________________________");
+        System.out.printf("\nEntre " + p_menor + " e " + p_maior + " multa de R$%1.2f", multa2);
+        System.out.print("\n___________________________________________");
+        System.out.printf("\nAcima de " + p_maior + " multa de R$%1.2f", multa3);
+        System.out.print(" por poluente emitido" + "\n___________________________________________");
+    }
+}
