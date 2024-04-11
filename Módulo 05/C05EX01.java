f8f9c9b82f57f17752630a6d800749d792c80924
Izabel <145824500+rainbowcrack@users.noreply.github.com>
2024-04-11 17:32:52 -0300

    Update C05EX01.java

--- Módulo 05/C05EX01.java
+++ Módulo 05/C05EX01.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
-// atividade do módulo 04 - exercício 05 por Izabel Oliveira da Paz Chaves (1º período de C.C)
+// atividade do modulo 05 - exercicio 01 por Izabel Oliveira da Paz Chaves (1 periodo de C.C)
 // programa de funcao de 3 grau quando o 2 termo eh zero, x dado pelo user
 public class C05EX01 {
     public static void main(String[] args) {
         // declaracao de variaveis
-        int x, fx;
+        double x, fx;
         Scanner teclado = new Scanner(System.in);
-        System.out.print("Para a expressao f(x) = x^3 + 4x + 10, informe o valor de um x inteiro: ");
-        x = teclado.nextInt();
+        System.out.print("Para a expressao f(x) = x^3 + 4x + 10, informe o valor de x: ");
+        x = teclado.nextDouble();
         // operacao e ouput
         // mudar x*x*x por Math.pow() no INTELIJI
-        fx = (x * x * x) + (4 * x) + 10;
-        System.out.print("F(x) = " + fx);
+        fx = Math.pow(x,3) + (4 * x) + 10;
+        System.out.printf("F(x) = %1.0f" + fx);
         teclado.close();
     }
 }
