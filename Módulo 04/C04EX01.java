1942558c4960e808c228eb309c67b4532fce849a
Izabel <145824500+rainbowcrack@users.noreply.github.com>
2024-04-11 17:21:48 -0300

    Update C04EX01.java

--- Módulo 04/C04EX01.java
+++ Módulo 04/C04EX01.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-
+// atividade do modulo 04 - exercicio 01 por Izabel Oliveira da Paz Chaves (1 periodo de C.C)
+// programa que formata a ordem de input do nome e do sobrenome e insere idade
 public class C04EX01 {
     public static void main(String[] args) {
         String nome1, nome2, nome3;
