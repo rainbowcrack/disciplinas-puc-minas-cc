027d500b1feb53cd94c4bfe275e8e467ba671bb9
Izabel <145824500+rainbowcrack@users.noreply.github.com>
2024-04-17 11:20:31 -0300

    Update C06EX02.java

--- Módulo 06/C06EX02.java
+++ Módulo 06/C06EX02.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
-
-// atividade do modulo 06 - exercicio 02 por Izabel Oliveira da Paz Chaves (1 periodo de C.C)
-// programa para imprimir o valor da multa a partir da emissao de poluentes
+// atividade do modulo 09 - exercicio 01 por Izabel Oliveira da Paz Chaves (1 periodo de C.C)
+// problemas sobre series matematicas baseadas em modelos ja prontos e looping
 public class C06EX02 {
     public static void main(String[] args){
+        // declarar e atribuir variavel
         // declarar variaveis e atribuir
         double poluente, multa;
         Scanner teclado = new Scanner(System.in);
         System.out.print("Para o calculo do valor da multa,\nIndique a quantidade de poluentes emitidos [mg/(t*m^2)]: ");
         poluente = teclado.nextDouble();
         // operacao e condicional
-        multa = poluente * 500;
+        multa = poluente * 5000; // criar a atribuicao da multa fora da condicional
         if (poluente <= 1500)
             System.out.print("Poluente = " + poluente + "\nMulta = R$0" );
 
