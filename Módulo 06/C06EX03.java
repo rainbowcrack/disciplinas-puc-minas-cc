861b9b44732d173732e7e4f4afc6e23a1939d1c3
Izabel <145824500+rainbowcrack@users.noreply.github.com>
2024-04-18 13:02:05 -0300

    Update C06EX03.java

--- Módulo 06/C06EX03.java
+++ Módulo 06/C06EX03.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
+
 // atividade do modulo 06 - exercicio 03 por Izabel Oliveira da Paz Chaves (1 periodo de C.C)
-// salario = 240 + comissao
-public class C05EX01 {
-    public static void main(String[] args){
+// programa para calcular o salario a partir de: salario = 240 + comissao
+public class C06EX03 {
+    public static void main(String[] args) {
         // definir variaveis e atribuir
-        double vendas, comissao;
+        double vendas;
         Scanner teclado = new Scanner(System.in);
         System.out.print("Para calcular o seu salario,\nInsira o valor mensal das vendas: R$");
         vendas = teclado.nextDouble();
         // condicional
-        comissao = 0.1 * vendas;
         if (vendas <= 1000)
-            System.out.print("Salario = R$240,00 ");
+            System.out.print("Salario = R$240,00 "); // valor fixo
 
-        if (vendas >= 1000 && vendas <= 10000)
-            System.out.print("Salario = " + vendas + comissao);
+        else if (vendas >= 1000 && vendas <= 10000)
+            System.out.printf("Salario = R$%1.2f", (240 + vendas * 0.1)); // valor variavel e formatado
 
         else
-            System.out.print("Salario =  " + vendas + 1000);
+            System.out.print("Salario = R$1240,00"); // valor fixo
         teclado.close();
 
     }
