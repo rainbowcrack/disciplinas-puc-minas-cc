e6e9fdc9d7f773fdb4778219292954fbc6684b6f
Izabel <145824500+rainbowcrack@users.noreply.github.com>
2024-04-21 13:36:00 -0300

    Update C07EX03.java

--- Módulo 07/C07EX03.java
+++ Módulo 07/C07EX03.java
@@ -7,26 +7,26 @@ public class C07EX03 {
         int dias;
         double imposto, multa;
         Scanner teclado = new Scanner(System.in);
-        System.out.print("Para calcular o valor da multa,\nIforme o imposto: R$");
+        System.out.print("Para calcular o valor da multa,\nInforme o imposto: R$");
         imposto = teclado.nextDouble();
         System.out.print("Os dias de atraso: ");
         dias = teclado.nextInt();
         // switch
         switch (dias) {
             case 1: case 2: case 3: case 4: case 5:
-                System.out.print("Multa = R$0");
+                System.out.print("\nMulta = R$0");
                 break;
             case 6: case 7: case 8:
-                multa = 1.002 * imposto;
-                System.out.print("Multa = R$0");
+                multa = 0.02 * imposto;
+                System.out.print("\nMulta = R$0");
                 break;
             case 9: case 10:
-                multa = (1.010 * imposto) + (1.0005 * dias);
-                System.out.printf("Multa = R$%1.2f", multa);
+                multa = (0.1 + (0.005 * dias)) * imposto;
+                System.out.printf("\nMulta = R$%1.2f", multa);
                 break;
             default:
                 multa = (1.5 * imposto) + dias;
-                System.out.printf("Multa = R$%1.2f", multa);
+                System.out.printf("\nMulta = R$%1.2f", multa);
                 break;            
         }
         teclado.close();
