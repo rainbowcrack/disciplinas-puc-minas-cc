a922c6762c708fb9251da65416ed7c4d888ee4b8
Izabel <145824500+rainbowcrack@users.noreply.github.com>
2024-04-21 14:41:49 -0300

    Update C06EX12.java

--- Módulo 06/C06EX12.java
+++ Módulo 06/C06EX12.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 // atividade do modulo 06 - exercicio 12 por Izabel Oliveira da Paz Chaves (1 periodo de C.C)
-// programa para calcular o custo das bolas produzidas ate a Copa
+// programa para calcular os custo de armazenagem e producao
 public class C06EX12 {
     public static void main(String[] args) {
-        // input e chamar biblioteca
-        int bolas, defeito, meses;
-        double p_papelao, aluguel, caixas, custo;
         Scanner teclado = new Scanner(System.in);
+        // variaveis e biblioteca
+        int bolas, defeito, meses, caixas;
+        double p_papelao, aluguel, galpoes, custoTotal, custoEmbalagem, custoEstocagem;
+        // input
         System.out.print("Para saber o custo total,\nInforme quantas bolas produzidas: ");
         bolas = teclado.nextInt();
         System.out.print("Dessas produzidas, informe quantas estao com defeito: ");
@@ -17,13 +18,18 @@ public class C06EX12 {
         aluguel = teclado.nextDouble();
         System.out.print("A quantidade de meses ate a Copa: ");
         meses = teclado.nextInt();
-        // output e operacao
-        bolas = bolas - defeito;
-        aluguel = aluguel * meses;
-        caixas = bolas;
-        p_papelao = p_papelao * caixas;
-        custo = aluguel + p_papelao;
-        System.out.printf("Custo total = R$%1.2f", custo);
+        // Calculo embalagem, estocagem e total
+        // Math.ceil converte o decimal para um inteiro proximo
+        bolas -= defeito; // descontando as bolas com defeito na variavel bolas
+        caixas = (int) Math.ceil((double) bolas / 10); // quantidade de caixas 
+        custoEmbalagem = p_papelao * caixas; // custo da embalagem
+        galpoes = Math.ceil((double) caixas / 850); // quantidade de galpoes 
+        custoEstocagem = aluguel * galpoes * meses; // o custo de estocagem
+        custoTotal = custoEmbalagem + custoEstocagem; // o custo total
+        // output
+        System.out.printf("\nCusto da embalagem: R$%.2f", custoEmbalagem);
+        System.out.printf("\nCusto de estocagem: R$%.2f", custoEstocagem);
+        System.out.printf("\nCusto total: R$%.2f", custoTotal);
         teclado.close();
     }
 }
